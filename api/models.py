17ccfa576185bb3610436e977247089631c90b81
João Pedro Volpi <joaopvolpi@gmail.com>
2019-12-03 16:55:20 -0300

    modelo palestra

--- api/models.py
+++ api/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 
 class Palestra(models.Model):
+    
     nome = models.CharField(max_length=100)
     palestrante = models.CharField(max_length=100)
     descricao = models.CharField(max_length=1000)
@@ -14,16 +15,16 @@ class Palestra(models.Model):
     def __str__(self):
         return self.nome + " - " + self.palestrante 
 
+'''
 class Form_Satisfacao(models.Model):
     id_palestra = models.ForeignKey(Palestra, on_delete=models.CASCADE)
-    
-    nome = models.CharField(max_length=100)
-    palestrante = models.CharField(max_length=100)
-    descricao = models.CharField(max_length=1000)
-    sala = models.CharField(max_length=20)
+
+    Pergunta1 = models.CharField(max_length=100)
+    Pergunta2 = models.CharField(max_length=100)
     
     data = models.DateTimeField()
 
     
     def __str__(self):
         return self.nome + " - " + self.palestrante 
+'''
--- api/urls.py
+++ api/urls.py
@@ -3,6 +3,6 @@ from .views import *
 
 
 urlpatterns = [
-    path("palestra/", PalestraList.as_view(), name="PalestraList"),
-    path("palestra/<int:pk>/", PalestraDetail.as_view(), name="PalestraDetail")
+    path("palestra/", PalestraList.as_view()),
+    path("palestra/<int:pk>/", PalestraDetail.as_view())
 ]
